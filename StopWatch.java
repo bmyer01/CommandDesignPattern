6e4d4f7567c2f21432947beabb2cab91fb791c99
Jordan duBarry <jordandubarry73@gmail.com>
2023-02-24 17:59:25 -0500

    Implemented StopWatch class

--- StopWatch.java
+++ StopWatch.java
@@ -1,3 +1,57 @@
+import java.util.concurrent.TimeUnit;
 public class StopWatch {
-    
+
+    public StopWatch() {
+        
+    }
+
+    public void countDownThirtySec() {
+        for(int i=29;i>=0;i--) {
+            try {
+                clearScreen();
+                System.out.println("00:"+timeNotation(i));
+                TimeUnit.MILLISECONDS.sleep(1000);
+            } catch (Exception e) {
+                
+            }
+        }
+    }
+
+    public void countDownOneMin() {
+        for(int i=59;i>=0;i--) {
+            try {
+                clearScreen();
+                System.out.println("00:"+timeNotation(i));
+                TimeUnit.MILLISECONDS.sleep(1000);
+            } catch (Exception e) {
+                
+            }
+        }
+    }
+
+    public void countDownFiveMin() {
+        for(int i = 4; i>=0; i--) {
+            for(int j=59;j>=0;j--) {
+                try {
+                    clearScreen();
+                    System.out.println("0"+i+":"+timeNotation(j));
+                    TimeUnit.MILLISECONDS.sleep(1000);
+                } catch (Exception e) {
+                    
+                }
+            }
+        }
+    }
+
+    private void clearScreen() {
+        System.out.print("\033[H\033[2J");
+        System.out.flush();
+    }
+
+    private String timeNotation(int num) {
+        if(num<10) {
+            return "0"+num;
+        }
+        return ""+num;
+    }
 }
