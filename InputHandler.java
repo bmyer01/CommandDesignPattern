399fb048859e06a02667becb6f79a23235063785
michaewcl <michaelwcluver@gmail.com>
2023-02-26 07:32:58 +0800

    In-Progress InputHandler things

--- InputHandler.java
+++ InputHandler.java
@@ -1,3 +1,21 @@
+import java.util.HashMap;
+/**
+ * A class to handle user input and pass it along to other classes
+ * @author Michael Cluver
+ */
 public class InputHandler {
-    
+    private HashMap<String, Command> commands;
+
+
+    public InputHandler(StopWatch Watch){
+        commands = new HashMap<String, Command>();
+        commands.put("1", new OneMinCommand());
+        commands.put("5", new FiveMinCommand());
+        commands.put("30",new ThirtySecCommand());
+
+    }
+
+    public boolean InputEntered(String data){
+
+    }
 }
